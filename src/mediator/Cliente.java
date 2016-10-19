f6dbae2f7b93bedb6fbebfd32325f9f018b2c8db
FACUL\QBEX <zellemos@hotmail.com>
2016-10-19 18:16:33 -0200

    commit de todo o projeto

---
+++ src/mediator/Cliente.java
@@ -0,0 +1,26 @@
+package mediator;
+
+import mediator.AndroidColleague;
+import mediator.IOSColleague;
+import mediator.MensagemMediator;
+import mediator.SymbianColleague;
+
+public class Cliente {
+	public static void main(String[] args) {
+		MensagemMediator mediador = new MensagemMediator();
+	
+		AndroidColleague android = new AndroidColleague(mediador);
+		IOSColleague ios = new IOSColleague(mediador);
+		SymbianColleague symbian = new SymbianColleague(mediador);
+	
+		mediador.adicionarColleague(android);
+		mediador.adicionarColleague(ios);
+		mediador.adicionarColleague(symbian);
+		
+		symbian.enviarMensagem("Oi, eu sou um Symbian!");
+		System.out.println("=========");
+		android.enviarMensagem("Oi Symbian! Eu sou um Android!");
+		System.out.println("=========");
+		ios.enviarMensagem("Olá todos, sou um iOs!");
+	}
+}
